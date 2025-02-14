d5d4425d6b3ce78ea7f15910ddd8f4d50232a353
ZacharyDangelo <zachdangelo100@gmail.com>
2025-02-14 14:52:25 -0500

    v01 add enemy spawner

---
+++ scripts/enemy_container.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+@export var num_to_spawn: int
+@export var min_offset: Vector2
+@export var max_offset: Vector2
+
+
+var camera
+var player
+
+func _ready():
+	camera = get_node('%Camera')
+	player = get_node('%Player')
+	
+func _process(delta):
+	if(get_children().size() == 0):
+		spawn_enemies()
+	
+func spawn_enemies():
+	print('Spawning enemies!')
+	
+	var camera_rect = camera.get_viewport_rect()  
+	var camera_position = camera.global_position  
+	var half_width = camera_rect.size.x / 2
+	var half_height = camera_rect.size.y / 2
+		
+	# Use camera's global position as the center point of the spawn area
+	var camera_center = camera.global_position  
+
+	for i in range(num_to_spawn):
+		var enemy = enemy_scene.instantiate()  
+		
+		# Spawn within the camera's visible area
+		var random_x = randf_range(camera_center.x - half_width + min_offset.x, 
+								   camera_center.x + half_width + max_offset.x)
+		var random_y = randf_range(camera_center.y - half_height + min_offset.y, 
+								   camera_center.y + half_height - max_offset.y)
+		
+		enemy.position = Vector2(random_x, random_y)  
+		enemy.camera = camera  # Ensure enemy has a reference to the camera
+		self.add_child(enemy)  
+
+	print("Finished spawning enemies")
--- scripts/weapons/projectile.gd
+++ scripts/weapons/projectile.gd
@@ -2,7 +2,15 @@ extends Node2D
 
 @export var speed: float
 @export var speed_mod: float
+@export var ttl: float
 
 
 func _process(delta):
 	position += Vector2(speed * delta,0)
+	ttl -= delta
+	if ttl <= 0:
+		self.queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	self.queue_free()
