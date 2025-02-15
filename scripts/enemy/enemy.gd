eab414c2afe2bb640809bb8b307577be9ba68a22
zachdangelo100@gmail.com <zachdangelo100@gmail.com>
2025-02-14 22:49:05 -0500

    set up parent scene for enemy and base movement script

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
+@export var sprite: Texture2D
+
+@export_group("Stats")
+@export var health: int
+@export var points: int
+
+
+
 var camera
+var movement
+
+func _ready():
+	get_node("Sprite").texture = sprite
+	movement = get_node("Movement")
 
 func _process(delta):
-	var camera_rect = camera.get_viewport_rect()  # Get camera's viewport rectangle
-	var camera_position = camera.global_position  # Get camera's position
-	var screen_left = camera_position.x - camera_rect.size.x / 2  # Left edge of the screen
-	var screen_right = camera_position.x + camera_rect.size.x / 2  # Right edge of the screen
-
-	if self.global_position.x < screen_left - 50 or self.global_position.x > screen_right + 50:
-		self.queue_free()
-
-func _on_hitbox_area_entered(area):
-	self.queue_free()
+	movement.process(delta)
+	movement.handle_out_of_bounds(camera)
