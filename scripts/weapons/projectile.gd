5b74c47bfe86a0cc22fa4ed224bc7ae15fa2ceec
ZacharyDangelo <zachdangelo100@gmail.com>
2025-02-27 19:53:06 -0500

    fix player speed and add laser animation

--- scripts/weapons/projectile.gd
+++ scripts/weapons/projectile.gd
@@ -4,12 +4,23 @@ extends Node2D
 @export var ttl: float
 @export var dir: Vector2 = Vector2.RIGHT
 
+func _ready():
+	var animation_player = get_node("AnimationPlayer")
+	if animation_player:
+		animation_player.play("laser")
+
 func _process(delta):
 	position += dir.rotated(rotation) * speed * delta
 	ttl -= delta
 	if ttl <= 0:
-		self.queue_free()
+		kill()
 
+func kill():
+	self.queue_free()
 
 func _on_area_2d_area_entered(area):
-	self.queue_free()
+	kill()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	kill()
